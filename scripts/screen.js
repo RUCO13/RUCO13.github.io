e3634e9d1b0998a9bd8184392a398236cbdc1ad9
RUCO13 <ruizoscar.1393@gmail.com>
2023-07-20 14:25:14 -0600

    Update

---
+++ scripts/screen.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Código relacionado con el canvas y su contexto aquí
+    var canvas = document.getElementById("tuCanvas");
+    var ctx = canvas.getContext("2d");
+    
+    // Resto del código relacionado con el canvas y su contexto aquí
+  });
+  
